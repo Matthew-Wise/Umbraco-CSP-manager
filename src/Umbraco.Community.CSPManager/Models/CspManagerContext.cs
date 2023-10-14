bc4a6848f0e5b6d9f64136ba3ed43209106d764d
Aaron <aaron@umbhost.net>
2023-10-14 15:03:12 +0100

    Removed as unrequired

--- src/Umbraco.Community.CSPManager/Models/CspManagerContext.cs
+++ src/Umbraco.Community.CSPManager/Models/CspManagerContext.cs
@@ -2,8 +2,6 @@
 
 public class CspManagerContext
 {
-	public bool StyleNonceEnabled { get; set; }
 	public string? StyleNonce { get; set; } = null;
-	public bool ScriptNonceEnabled { get; set; }
 	public string? ScriptNonce { get; set; } = null;
 }
