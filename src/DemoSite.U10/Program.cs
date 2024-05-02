449edf996ed62a064ef896a3cf3ebd229606fc11
Matthew Wise <6782865+Matthew-Wise@users.noreply.github.com>
2024-05-02 11:29:45 +0100

    Targeting v13

---
+++ src/DemoSite.U10/Program.cs
@@ -0,0 +1,18 @@
+namespace DemoSite;
+
+public class Program
+{
+	public static void Main(string[] args)
+		=> CreateHostBuilder(args)
+			.Build()
+			.Run();
+
+	public static IHostBuilder CreateHostBuilder(string[] args) =>
+		Host.CreateDefaultBuilder(args)
+			.ConfigureUmbracoDefaults()
+			.ConfigureWebHostDefaults(webBuilder =>
+			{
+				webBuilder.UseStaticWebAssets();
+				webBuilder.UseStartup<Startup>();
+			});
+}
