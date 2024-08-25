18450183cd00c10735ead71bdc163e9f4639f5d0
Matthew Wise <6782865+Matthew-Wise@users.noreply.github.com>
2024-08-25 16:39:50 +0100

    Updated with v13 features targetting 14.2.0

---
+++ src/Umbraco.Community.CSPManager.Core/Notifications/CspDistCacheRefresherNotification.cs
@@ -0,0 +1,13 @@
+﻿namespace Umbraco.Community.CSPManager.Core.Notifications;
+
+using Umbraco.Cms.Core.Notifications;
+using Umbraco.Cms.Core.Sync;
+
+public class CspDistCacheRefresherNotification
+	: CacheRefresherNotification
+{
+	public CspDistCacheRefresherNotification(
+		object messageObject, 
+		MessageType messageType
+	) : base(messageObject, messageType) { }
+}
